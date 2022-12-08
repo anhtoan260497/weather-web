d4a28d0c15f3df4f0e7815ada580ecb2901ba8ef
anhtoan260497 <anhtoan260497@gmail.com>
2022-12-08 20:01:54 +0700

    replace files

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './styles.scss'
+// import PropTypes from 'prop-types';
+
+// Header.propTypes = {
+    
+// };
+
+function Header(props) {
+    return (
+        <div id='header'>
+            <h1>dasdsadsads</h1>
+        </div>
+    );
+}
+
+export default Header;
